7ca81f88acbae2684c7d7c43a6b41d0e0bd4be4f
garzuzo <garzuxo@gmail.com>
2019-08-24 10:08:08 -0500

    add HEAD, PATCH, PUT, DELETE test services

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -18,32 +18,90 @@ describe('First Api Tests', () => {
 
 
       it('Consume GET Service with query parameters', async () => {
-        const query = {
+        const reqBody = {
           name: 'John',
           age: '31',
           city: 'New York'
         };
       
-        const response = await agent.get('https://httpbin.org/get').query(query);
+        const response = await agent.get('https://httpbin.org/get').query(reqBody);
       
         expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.args).to.eql(query);
+        expect(response.body.args).to.eql(reqBody);
       });
 
 
       it('Consume POST Service with query params', async ()=>{
-        const query={
+        const reqBody={
             bestSkill: 'NodeJs'
 
         };
 
-        const response = await agent.post('https://httpbin.org/post').query(query);
+        const response = await agent.post('https://httpbin.org/post').query(reqBody);
 
         expect(response.status).to.equal(statusCode.OK);
-        expect(response.body.args).to.eql(query);
+        expect(response.body.args).to.eql(reqBody);
 
 
       });
+
+
+      it('Consume HEAD Service with query params', async ()=>{
+
+        const reqBody={
+            username: 'garzuzo'
+        };
+
+        const response = await agent.head('https://httpbin.org/get').query(reqBody);
+        expect(response.status).to.equal(statusCode.OK);
+    
+
+      });
+
+
+
+      it('Consume PATCH Service with query params', async ()=>{
+
+        const reqBody={
+username:"jgarzon"
+        };
+
+        const response = await agent.patch('https://httpbin.org/patch').query(reqBody);
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.args).to.eql(reqBody);
+
+      });
+
+
+      it('Consume PUT Service with query params', async ()=>{
+
+        const reqBody={
+            username:"JohnatanG"
+        };
+
+        const response = await agent.put('https://httpbin.org/put').query(reqBody);
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.args).to.eql(reqBody);
+
+      });
+
+
+
+      it('Consume DELETE Service with query params', async ()=>{
+
+        const reqBody={
+username:"garzuzo"
+        };
+
+        const response = await agent.delete('https://httpbin.org/delete').query(reqBody);
+        expect(response.status).to.equal(statusCode.OK);
+        expect(response.body.args).to.eql(reqBody);
+
+      });   
+
+
+
+
 });
 
 
